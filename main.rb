65c553b13f03059233041755cb31e03c299b2139
Roseanne23 <roseanne.gulosino@gmail.com>
2024-09-19 18:01:51 +0800

    import student class and add student method

--- main.rb
+++ main.rb
@@ -1,1 +1,23 @@
-puts "Hello World"
+require_relative 'student'
+def new_student
+  puts "Add new student"
+  student_id = Student.all.size + 1
+  puts "Enter student name:"
+  student_name = gets.chomp
+  puts "Enter student birth date:"
+  student_birth_date = gets.chomp
+  puts "Enter student email:"
+  student_email = gets.chomp
+  puts "Enter student phone number:"
+  student_phone_number = gets.chomp
+
+  student=Student.new(student_id, student_name, student_birth_date, student_email, student_phone_number)
+  student.save
+
+  if Student.find(student_id)
+    puts "Student added successfully!"
+    student.display
+  end
+end
+
+new_student
