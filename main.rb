4baba2f9c3723c8385e488ca40ac0d5ed2252a45
Roseanne23 <roseanne.gulosino@gmail.com>
2024-09-19 22:54:06 +0800

    updated delete_student method and spacing

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require_relative 'student'
 
 def new_student
-  puts "Add new student"
+  puts"Add new student"
   student_id = Student.all.size + 1
   puts "Enter student name:"
   student_name = gets.chomp
@@ -12,7 +12,7 @@ def new_student
   puts "Enter student phone number:"
   student_phone_number = gets.chomp
 
-  student=Student.new(student_id, student_name, student_birth_date, student_email, student_phone_number)
+  student =Student.new(student_id, student_name, student_birth_date, student_email, student_phone_number)
   student.save
 
   if Student.find(student_id)
@@ -22,35 +22,37 @@ def new_student
 end
 
 new_student
+
 def delete_student
   puts "Delete a student"
   puts "Input student ID you want to delete:"
   student_id = gets.chomp.to_i
-  student = Student.find(student_id)
- if student.destroy
-  unless Student.find(student_id)
-  puts "Student destroyed successfully!"
+
+  if student = Student.find(student_id)
+     student.destroy
+     puts "Student destroyed successfully!"
   end
- end
 end
+
 def menu
   while true
-  puts "Choose an action:"
-  puts "1. Add a new student"
-  puts "2. Delete a student"
-  puts "3. Exit"
-  answer = gets.chomp.to_i
-  case answer
-  when 1
+    puts "Choose an action:"
+    puts "1. Add a new student"
+    puts "2. Delete a student"
+    puts "3. Exit"
+    answer = gets.chomp.to_i
+
+    case answer
+    when 1
     added_student
-  when 2
+    when 2
     delete_student
-  when 3
+    when 3
     puts "Exit program"
-  break
-  else
+    break
+    else
     puts "Student not found"
-  end
+    end
   end
 end
 menu
