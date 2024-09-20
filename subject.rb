0f98c872ea4141eb62d27e24678081196a8308ed
Roseanne23 <>
2024-09-20 11:18:29 +0800

    created subject class

---
+++ subject.rb
@@ -0,0 +1,28 @@
+class Subject
+  attr_accessor :id, :name
+
+  @@record = []
+  def initialize(id, name)
+   @id = id
+   @name = name
+  end
+  def save
+   @@record.prepend(self)
+  end
+
+  def destroy
+   @@record.delete(self)
+  end
+
+  def display
+    "ID: #{id}, Name: #{name}"
+  end
+
+  def self.all
+    @@record
+  end
+
+  def self.find(id)
+    @@record.find { |subject| subject.id == id}
+  end
+end
