f62ff039ac1ef1c8dd9c056e1883acd84ebeab57
Roseanne23 <>
2024-09-20 08:39:13 +0800

    add a new course and course management action

--- main.rb
+++ main.rb
@@ -1,5 +1,21 @@
 require_relative 'student'
+require_relative 'course'
 
+def new_course
+  puts "Add new course"
+  course_id = Course.all.size + 1
+  puts "Add a new course:"
+  course_name = gets.chomp
+  course = Course.new(course_id, course_name)
+  course.save
+
+  if Course.find(course_id)
+    puts "Course added successfully!"
+    puts course.display
+  else
+    puts "Course not added"
+  end
+end
 def new_student
   puts"Add new student"
   student_id = Student.all.size + 1
@@ -39,19 +55,38 @@ def menu
     puts "Choose an action:"
     puts "1. Add a new student"
     puts "2. Delete a student"
-    puts "3. Exit"
+    puts "3. Add a new course"
+    puts "4. Exit"
     answer = gets.chomp.to_i
 
     case answer
     when 1
-    added_student
+      new_student
     when 2
-    delete_student
+      delete_student
     when 3
-    puts "Exit program"
+      new_course
+    when 4
+      puts "Exit program"
     break
     else
     puts "Student not found"
+    end
+  end
+end
+def course_management
+  while true
+    puts "Course Management"
+    puts "1. Add a new course"
+    puts "2. Exit"
+    answer = gets.chomp.to_i
+
+  case answer
+    when 1
+      new_course
+    when 2
+      puts "Exit program"
+      break
     end
   end
 end
