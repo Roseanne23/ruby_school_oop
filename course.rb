b0e5ad8c9e6834824f5d47c16a73d176e9210c56
Roseanne23 <roseanne.gulosino@gmail.com>
2024-09-23 07:56:45 +0800

    modified save method in course class

--- course.rb
+++ course.rb
@@ -1,14 +1,22 @@
 class Course
   attr_accessor :id, :name, :deleted_at
-  def initialize(id, name, deleted_at = nil)
+  def initialize(id, name)
    @id = id
    @name = name
-   @deleted_at = deleted_at
+   @deleted_at = nil
   end
   @@record = []
 
   def save
-   @@record.prepend(self)
+    existing_record = @@record.find { |course| course.id == @id }
+    if existing_record
+      existing_record.name = @name
+      existing_record.deleted_at = @deleted_at
+      puts "Course updated successfully!"
+    else
+      @@record << self
+      puts "Course added successfully!"
+    end
   end
 
   def destroy
